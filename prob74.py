aa636773cc9f81f533fd7cc56c38897b3f1b8457
Prithviraj billa <prithviraj.m.billa@gmail.com>
2014-05-07 17:11:03 +0530

    completed prob74.py

--- prob74.py
+++ prob74.py
@@ -9,8 +9,19 @@ def sum_of_fact(i):
 	while i > 0:
 		num = i%10
 		answer= answer+factorials[num]
-		num=num/10
+		i=i/10
 	return answer
 
+a = 0
 for i in range(1,1000000):
 	li = []
+	k = i
+	while k not in li:
+		li.append(k)
+		k = sum_of_fact(k)
+
+	if len(li) == 60:
+		a= a+1
+	# print i
+
+print a
