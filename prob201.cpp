b1ec0569dcae747561df8bb6bfe86f518d5d4067
billa <prithviraj.m.billa@gmail.com>
2014-05-09 17:16:08 +0530

    I have to be productive
    atlast completed 75. level 3 \m/

---
+++ prob201.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long int A[100];
+	for (int i = 0; i < 100; ++i)
+	{
+		a[i] = (i+1)*(i+1);
+	}
+	set<long long int> sum;
+	
+
+	return 0;
+}
