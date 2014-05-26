4be7cde28bdf27d2c557f50080ea3594c7a7a9de
billa <prithviraj.m.billa@gmail.com>
2014-05-26 16:45:45 +0530

    i have no idea what this is

---
+++ prob82.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	long long int array[80][80];
+	for (int i = 0; i < 80; ++i)
+	{
+		for (int j = 0; j < 80; ++j)
+		{
+			cin>>array[i][j];
+		}
+	}
+	
+	return 0;
+}
